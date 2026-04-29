32d7171ab1c3da265029c8a2c62d44017555a2cb
Qi Li <139032475+lixiaoben1@users.noreply.github.com>
2026-04-29 17:11:49 +0800

    删掉注释

--- src/views/login_page/index.vue
+++ src/views/login_page/index.vue
@@ -61,7 +61,7 @@ const toggleToChildren = ()=>{
   --circle-size: 80%;
   --blending: hard-light;
 }
-//background-image: linear-gradient( 109.6deg,  rgba(112,246,255,0.33) 11.2%, rgba(221,108,241,0.26) 42%, rgba(229,106,253,0.71) 71.5%, rgba(123,183,253,1) 100.2% );
+
 
 * {
   margin:0;
